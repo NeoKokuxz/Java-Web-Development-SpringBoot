a678f919031a429efcc1e04e7ef8909db08b30f4
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-07-08 00:15:22 -0400

    initial Spring project update

---
+++ Java-Web-Springboot-Project-1/src/main/java/com/neokokuxz/JavaWebSpringboot/JavaWebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.neokokuxz.JavaWebSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebSpringbootApplication.class, args);
+	}
+
+}
---
+++ Java-Web-Springboot-Project-1/src/test/java/com/neokokuxz/JavaWebSpringboot/JavaWebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neokokuxz.JavaWebSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

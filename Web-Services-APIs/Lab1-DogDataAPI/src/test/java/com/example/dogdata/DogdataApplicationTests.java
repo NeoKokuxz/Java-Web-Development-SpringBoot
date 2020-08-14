6f4db8a154681c2de704a1d09d4d21d5585b01f1
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-08-13 22:22:51 -0400

    Vehicle Project initial updated and Dog API MockMVC Test updated

--- Web-Services-APIs/Lab1-DogDataAPI/src/test/java/com/example/dogdata/DogdataApplicationTests.java
+++ Web-Services-APIs/Lab1-DogDataAPI/src/test/java/com/example/dogdata/DogdataApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.dogdata;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DogdataApplicationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    public void getAllDogs(){
+        ResponseEntity<List> response =
+                this.restTemplate.withBasicAuth("admin", "password").getForEntity("http://localhost:" + port + "/dogs", List.class);
+        System.out.println(response);
+
+        assertThat(response.getStatusCode(), Matchers.equalTo(HttpStatus.OK));
     }
 
+    @Test
+    public void getDog(){
+        ResponseEntity<String> response =
+                this.restTemplate.withBasicAuth("admin", "password").getForEntity("http://localhost:" + port + "/dogs/breed/1", String.class);
+
+        assertThat(response.getStatusCode(), Matchers.equalTo(HttpStatus.OK));
+    }
 }

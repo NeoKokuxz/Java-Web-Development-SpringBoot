64a2b233f41e212b8b0c5c7bf3c9493934343ba4
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-10-13 21:22:54 -0400

    Test insert for MySQL

--- Core Projects/Critter Chronologer/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
+++ Core Projects/Critter Chronologer/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
@@ -1,11 +1,16 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.pet.entity.Pet;
+import com.udacity.jdnd.course3.critter.pet.service.PetService;
 import com.udacity.jdnd.course3.critter.user.customer.entity.CustomerDTO;
 import com.udacity.jdnd.course3.critter.user.employee.dto.EmployeeDTO;
 import com.udacity.jdnd.course3.critter.user.employee.dto.EmployeeRequestDTO;
+import com.udacity.jdnd.course3.critter.user.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -19,39 +24,87 @@ import java.util.Set;
 @RequestMapping("/user")
 public class UserController {
 
+
+    @Autowired
+    private PetMapper petMapper;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private PetService petService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private  EmployeeService employeeService;
+
     @PostMapping("/customer")
-    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
-        throw new UnsupportedOperationException();
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        List<Long> petIds = customerDTO.getPetIds();
+        List<Pet> pets = new ArrayList<>();
+        if (petIds != null) {546
+            for (Long petId : petIds) {
+                Pet pet = petService.getPet(petId);
+                pets.add(pet);
+            }
+        }
+        Customer customer = customerMapper.DTOtoEntity(customerDTO);
+        customer.setPets(pets);
+
+        Customer savedCustomer = customerService.createCustomer(customer);
+        return customerMapper.entityToDTO(savedCustomer);
     }
 
     @GetMapping("/customer")
-    public List<CustomerDTO> getAllCustomers(){
-        throw new UnsupportedOperationException();
+    public List<CustomerDTO> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return customerMapper.entitiesToDTOs(customers);
     }
 
     @GetMapping("/customer/pet/{petId}")
-    public CustomerDTO getOwnerByPet(@PathVariable long petId){
-        throw new UnsupportedOperationException();
+    public CustomerDTO getOwnerByPet(@PathVariable long petId) {
+        Pet pet = petService.getPet(petId);
+        if (pet != null) {
+            if (pet.getCustomer() != null) {
+                Customer customer = customerService.getCustomer(pet.getCustomer().getId());
+                return customerMapper.entityToDTO(customer);
+            }
+        }
+        return null;
     }
 
     @PostMapping("/employee")
     public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        Employee employee = employeeMapper.DTOtoEntity(employeeDTO);
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        return employeeMapper.entityToDTO(savedEmployee);
     }
 
     @PostMapping("/employee/{employeeId}")
     public EmployeeDTO getEmployee(@PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        Employee employee = employeeService.getEmployee(employeeId);
+        return employeeMapper.entityToDTO(employee);
     }
 
     @PutMapping("/employee/{employeeId}")
     public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        Employee employee = employeeService.getEmployee(employeeId);
+        if (employee != null) {
+            employee.setDaysAvailable(daysAvailable);
+            employeeService.saveEmployee(employee);
+        }
     }
 
     @GetMapping("/employee/availability")
     public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
-        throw new UnsupportedOperationException();
+        List<Employee> employees = employeeService.getEmployeeByService(employeeDTO.getSkills(), employeeDTO.getDate());
+        return employeeMapper.entitiesToDTOs(employees);
     }
+
 
 }
